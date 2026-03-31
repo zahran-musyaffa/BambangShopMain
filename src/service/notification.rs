6a26c95f707c28c7e127bcff8646a351e654b28d
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2026-03-31 11:04:25 +0700

    Implement subscribe function in Notification controller

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -6,7 +6,7 @@ use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
-
+    
 pub struct NotificationService;
 
 impl NotificationService {
